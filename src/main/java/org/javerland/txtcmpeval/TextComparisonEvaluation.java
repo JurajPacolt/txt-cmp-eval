c9648dad0066b1dd5c98ad42ff838d7ff9578313
Juraj Pacolt <JurajPacolt@github.com>
2022-01-03 21:13:09 +0100

    first commit ...

---
+++ src/main/java/org/javerland/txtcmpeval/TextComparisonEvaluation.java
@@ -0,0 +1,9 @@
+package org.javerland.txtcmpeval;
+
+public class TextComparisonEvaluation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
